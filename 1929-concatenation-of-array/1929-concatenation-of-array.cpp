9b05101f5265514d9d7bbd5af0f32e13dbdd3987
supratim531 <91423964+supratim531@users.noreply.github.com>
2022-09-19 09:36:59 +0530

    Time: 13 ms (68.12%), Space: 12.6 MB (70.38%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -5,7 +5,7 @@ public:
         int N = n + n;
         vector<int> ans(N);
         for (int i = 0; i < n; ++i)
-            ans[i] = nums[i], ans[i + n] = nums[i];
+            ans[i] = ans[i + n] = nums[i];
         return ans;
     }
 };
