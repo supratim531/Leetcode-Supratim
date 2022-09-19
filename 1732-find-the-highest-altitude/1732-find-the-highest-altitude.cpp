e0661ff798cbc07c04b325a5b3217e0d1201ac78
supratim531 <91423964+supratim531@users.noreply.github.com>
2022-09-20 01:06:17 +0530

    Time: 5 ms (34.45%), Space: 8 MB (76.50%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int n = gain.size();
-        vector<int> ans(n + 1);
-        ans[0] = 0;
-        for (int i = 0; i < n; ++i)
-            ans[i + 1] = gain[i] + ans[i];
-        int hgst_alt = *max_element(ans.begin(), ans.end());
-        return hgst_alt;
+        int ans = max(0, gain[0]);
+        for (int i = 1; i < gain.size(); ++i)
+            gain[i] += gain[i - 1], ans = max(ans, gain[i]);
+        return ans;
     }
 };
