21678bd0baa28e6b80f0983ee884713aa74082aa
supratim531 <91423964+supratim531@users.noreply.github.com>
2022-09-26 02:09:10 +0530

    Time: 6 ms (13.53%), Space: 6.5 MB (54.82%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,41 @@
+int hsh[26];
+map<string, char> mp;
+
+bool present(string &p) {
+    return mp.find(p) != mp.end();
+}
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        int space = 0;
+        for (auto &ele : s)
+            if (ele == ' ')
+                space++;
+        if (pattern.size() - 1 != space) return false;
+        int i = 0;
+        mp.clear();
+        string proxy = "";
+        for (int c = 0; c < 26; c++)
+            hsh[c] = 0;
+        for (int c = 0; c <= s.size(); c++) {
+            char ele = s[c];
+            if (ele == ' ' || ele == '\0') {
+                if (present(proxy)) {
+                    if (mp[proxy] == pattern[i]);
+                    else return false;
+                } else {
+                    if (hsh[pattern[i] - 'a']) return false;
+                    mp.insert({proxy, pattern[i]});
+                }
+                hsh[pattern[i] - 'a']++;
+                proxy = "";
+                ++i;
+            }
+            else {
+                proxy.push_back(ele);
+            }
+        }
+        return true;
+    }
+};
