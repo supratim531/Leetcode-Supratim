b0a46e151cfb32a26a0eac2ee964729c115f8ae0
supratim531 <91423964+supratim531@users.noreply.github.com>
2022-09-25 09:14:58 +0530

    Time: 34 ms (10.75%), Space: 67.8 MB (5.68%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0, N = 100000;
+        int[] hsh = new int[N];
+        Integer[] nums2 = new Integer[nums.length];
+        
+        for (int i = 0; i < nums.length; ++i)
+            nums2[i] = nums[i];
+
+        int least = Collections.min(Arrays.asList(nums2));
+        least = (least < 0) ? least * -1 : least;
+        
+        for (int ele : nums2)
+            hsh[ele + least] += 1;
+        
+        for (int ele : nums2) {
+            if (hsh[ele + least] == 1) {
+                ans = ele;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+}
