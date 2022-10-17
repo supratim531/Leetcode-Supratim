9b3a76a44a1667fe6caf58548119a56eaf665b7f
supratim531 <91423964+supratim531@users.noreply.github.com>
2022-10-17 20:07:43 +0530

    Time: 28 ms (38.38%), Space: 9.7 MB (70.00%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -4,9 +4,9 @@ public:
         int ans = 0;
         for (auto &ele : sentences) {
             int space_count = 1;
-            for (int i = 0; i < ele.size(); ++i) {
-                if (ele[i] == ' ') space_count++;
-            }
+            for (int i = 0; i < ele.size(); ++i)
+                if (ele[i] == ' ')
+                    space_count++;
             ans = max(ans, space_count);
         }
         return ans;
